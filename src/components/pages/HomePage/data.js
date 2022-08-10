887fd457b924c323c8193c80ca7f66ee899c84a7
Austin Asoluka <stracool9@gmail.com>
2022-08-10 12:26:58 +0100

    update codebase

---
+++ src/components/pages/HomePage/data.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    img: "https://cdn.pixabay.com/photo/2019/04/14/14/09/fantasy-4126847_960_720.jpg",
+    title: "The woman that kills..",
+  },
+  {
+    img: "https://cdn.pixabay.com/photo/2014/08/22/09/36/auto-424119_960_720.jpg",
+    title: "The man that heals..",
+  },
+];
+
+export default data;
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Input } from "../../atoms";
 import { Footer, NavBar } from "../../molecules";
 import { MovieList } from "../../organisms";
 import { PageTemplate } from "../../templates/PageTemplate";
+import movieData from "./data";
 
 export const HomePage = () => {
+  const [filter, setFilter] = useState("");
+
+  const handleInputChange = (e) => {
+    setFilter(e.target.value);
+  };
+
   return (
     <PageTemplate header={<NavBar />} footer={<Footer />}>
+      {console.log(filter)}
       <div className="content h-50">
-        <Input type="search" />
+        <Input type="search" onChange={handleInputChange} />
 
         <div className="mt-5">
-          {[
-            {
-              img: "https://cdn.pixabay.com/photo/2019/04/14/14/09/fantasy-4126847_960_720.jpg",
-              title: "The woman that kills..",
-            },
-            {
-              img: "https://cdn.pixabay.com/photo/2014/08/22/09/36/auto-424119_960_720.jpg",
-              title: "The man that heals..",
-            },
-          ].map((elem, idx) => (
-            <MovieList key={idx} img={elem.img} title={elem.title} />
-          ))}
+          {movieData
+            .filter((elem) =>
+              elem.title.toLowerCase().includes(filter.toLowerCase())
+            )
+            .map((elem, idx) => {
+              return <MovieList key={idx} img={elem.img} title={elem.title} />;
+            })}
         </div>
       </div>
     </PageTemplate>
