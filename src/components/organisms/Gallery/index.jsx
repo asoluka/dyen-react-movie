9bf676c4752a6f3c219fadeb7be486d8fd6b2bbb
Austin Asoluka <stracool9@gmail.com>
2022-08-19 11:52:44 +0100

    demo prop-drilling with a very quacked implementation of slider

--- src/components/organisms/Gallery/index.jsx
+++ src/components/organisms/Gallery/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Text } from "../../atoms";
 
-export const Gallery = ({ title, images }) => {
+export const Gallery = ({ title, images, next, prev }) => {
   return (
     <>
       <div className="container grid grid-column-3">
-        {images.map((image) => image)}
+        {images.map((image, idx) => (
+          <div key={idx}>{image}</div>
+        ))}
       </div>
 
       <div className="container d-flex align-items-center content-space-between">
@@ -14,17 +16,29 @@ export const Gallery = ({ title, images }) => {
           <Text className="bold grey">01/05 Coffee Shop</Text>
         </div>
         <div className="d-flex">
-          <div className="hamburger d-flex align-items-center content-center mr-2">
-            <div className="">
-              <div className="hamburger-bars"></div>
-              <div className="hamburger-bars"></div>
-            </div>
+          <div
+            onClick={() =>
+              prev([
+                <div className="bg-1"></div>,
+                <div className="bg-2"></div>,
+                <div className="bg-3"></div>,
+              ])
+            }
+            className="hamburger d-flex align-items-center content-center mr-2"
+          >
+            <div className="">prev</div>
           </div>
-          <div className="hamburger d-flex align-items-center content-center">
-            <div className="">
-              <div className="hamburger-bars"></div>
-              <div className="hamburger-bars"></div>
-            </div>
+          <div
+            onClick={() =>
+              next([
+                <div className="bg-4"></div>,
+                <div className="bg-5"></div>,
+                <div className="bg-6"></div>,
+              ])
+            }
+            className="hamburger d-flex align-items-center content-center"
+          >
+            <div className="">next</div>
           </div>
         </div>
       </div>
--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavBar } from "../../molecules";
 import { Gallery } from "../../organisms/Gallery";
 import { HeroSection } from "../../organisms/HeroSection";
 import { PageTemplate } from "../../templates/PageTemplate";
 
 export const HomePage = () => {
+  const [images, setImages] = useState([
+    <div className="bg-1"></div>,
+    <div className="bg-2"></div>,
+    <div className="bg-3"></div>,
+  ]);
+
   return (
     <PageTemplate header={<NavBar />}>
       <HeroSection />
 
       <Gallery
         title="Our Last Projects"
-        images={[
-          <div className="bg-1"></div>,
-          <div className="bg-2"></div>,
-          <div className="bg-3"></div>,
-        ]}
+        images={images}
+        next={setImages}
+        prev={setImages}
       />
     </PageTemplate>
   );
